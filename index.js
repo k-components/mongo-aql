953192dfe8348e5fecf3f1b75f63a42f06c21db9
Ilkka Huotari <ilkkah@gmail.com>
2021-07-02 14:52:29 +0300

    Restructuring +

--- index.js
+++ index.js
@@ -2,23 +2,23 @@
 require('./lib/normalize');
 
 // Register query types
-require('./helpers/query-types');
+require('mongo-aql/lib/helpers/query-types');
 
 // Register query helpers
-require('./helpers/query/values');
-require('./helpers/query/order');
-require('./helpers/query/limit');
-require('./helpers/query/embed');
-require('./helpers/query/offset');
-require('./helpers/query/alias');
-require('./helpers/query/columns');
-require('./helpers/query/table');
-require('./helpers/query/where');
-require('./helpers/query/return');
-require('./helpers/query/text');
+require('mongo-aql/lib/helpers/query/values');
+require('mongo-aql/lib/helpers/query/order');
+require('mongo-aql/lib/helpers/query/limit');
+require('mongo-aql/lib/helpers/query/embed');
+require('mongo-aql/lib/helpers/query/offset');
+require('mongo-aql/lib/helpers/query/alias');
+require('mongo-aql/lib/helpers/query/columns');
+require('mongo-aql/lib/helpers/query/table');
+require('mongo-aql/lib/helpers/query/where');
+require('mongo-aql/lib/helpers/query/return');
+require('mongo-aql/lib/helpers/query/text');
 
 // Register conditional helpers
-require('./helpers/conditional');
+require('mongo-aql/lib/helpers/conditional');
 
 module.exports = require('./lib/query-builder');
 module.exports.neighbors = require('./lib/query-builder-graph').neighbors;
